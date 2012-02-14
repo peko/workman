4fd967299e852ef2504a5588f0c2e34d77b41d9c
peko <peko@asus.(none)>
2012-02-14 03:31:11 -0600

    ru/eng installer

--- Linux/xkb/modified/install.sh
+++ Linux/xkb/modified/install.sh
@@ -1,4 +1,4 @@
 sudo cp -rf ru /usr/share/X11/xkb/symbols/
 sudo cp -rf us /usr/share/X11/xkb/symbols/
 
-setxkbmap -v "us(workman)" && xset r 66
+setxkbmap -layout 'us(colemak),ru(phonetic)' -option 'grp:alt_shift_toggle' && xset r 66 &
