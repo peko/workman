c97020fe4d553470dfc1858c2cd8c5ed12c87248
peko <peko@asus>
2012-02-15 15:22:55 -0500

    profile

--- Linux/xkb/modified/install.sh
+++ Linux/xkb/modified/install.sh
@@ -2,3 +2,4 @@ sudo cp -rf ru /usr/share/X11/xkb/symbols/
 sudo cp -rf us /usr/share/X11/xkb/symbols/
 
 setxkbmap -layout 'us(colemak),ru(phonetic)' -option 'grp:alt_shift_toggle' && xset r 66 &
+echo "setxkbmap -layout 'us(colemak),ru(phonetic)' -option 'grp:alt_shift_toggle' && xset r 66 &" >> ~/.profile
