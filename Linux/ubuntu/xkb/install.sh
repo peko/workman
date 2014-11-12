f94237a103717f5039b7aec23196f493572dcdf9
peko <peko@gasubasu.com>
2014-11-12 05:55:58 +0400

    patching installation, Cyrillic_IO fix

--- Linux/ubuntu/xkb/install.sh
+++ Linux/ubuntu/xkb/install.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 # copy phonetic workman variant to russian symbols
-sudo cat ./symbols/workman_ru >> /usr/share/X11/xkb/symbols/ru 
+#sudo cat ./symbols/workman_ru >> /usr/share/X11/xkb/symbols/ru 
+
+sudo patch -p1 -i ./symbols/ru.patch
+# ubuntu have workman allready
+# patch --dry-run -p1 -i ./symbols/en.patch
+sudo patch -p1 -i ./rules/evdev.lst.patch
+sudo patch -p1 -i ./rules/evdev.xml.patch
 
 # clean cache
 sudo rm /var/lib/xkb/*
