013845925d494a14d37e08e6c17e915d0b8973d1
peko <peko@gasubasu.com>
2015-03-11 10:26:39 +0300

    back to inocence

---
+++ Linux/ubuntu/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# patch creation
+# diff -rupN /usr/share/X11/xkb/ new/ > xkb_ru.patch
+
+# applaing patch
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd /
+sudo patch -p0 < $DIR/xkb_ru.patch
