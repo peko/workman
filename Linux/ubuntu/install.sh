fc94cc19ed39f76041dd61ea6a30cf213394d4d2
peko <peko@gasubasu.com>
2015-03-11 10:26:53 +0300

    gramar

--- Linux/ubuntu/install.sh
+++ Linux/ubuntu/install.sh
@@ -3,7 +3,7 @@
 # patch creation
 # diff -rupN /usr/share/X11/xkb/ new/ > xkb_ru.patch
 
-# applaing patch
+# applying patch
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd /
 sudo patch -p0 < $DIR/xkb_ru.patch
